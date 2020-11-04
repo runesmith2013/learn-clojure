feae9cfffe53f7f6bc01a1068223d533fc0d260b
Robert Barr <robertbarr.uk@gmail.com>
2020-11-04 12:02:43 +0000

    collections examples

--- src/learning_clojure/chapter4_core_functions/collections.clj
+++ src/learning_clojure/chapter4_core_functions/collections.clj
@@ -19,4 +19,62 @@
 ;; CONJ
 ;;=====
 
-(conj [0] [1])
+(conj [0] [1])                                              ; [0 [1]]
+(conj [0] 1)                                                ; [0 1]
+
+(conj [0] 1 2 3 4)
+(conj {:time "midnight"} [:place "ye olde"])
+
+(defn my-conj
+  [target & additions]
+  (into target additions))
+
+
+;; function functions
+;====================
+
+; apply - explodes a seqable data struct so it can be passed to a fun
+; -----
+(max 0 1 2)                                                 ; 2
+(max [0 1 2])                                               ; [0 1 2]
+(apply max [0 1 2])                                         ; 2
+
+; defining into with conj and apply
+(defn my-into
+  [target additions]
+  (apply conj target additions))
+
+; partial
+; ------
+; takes a function and any number of args, returns new function
+; when new function called, call old function with original args and new args
+(def add10 (partial + 10))
+(add10 3)                                                   ; 13
+(add10 5)                                                   ; 15
+
+(def add-missing0elements
+  (partial conj ["Water" "eart" "air"]))
+
+(add-missing0elements "unobtanium" "adamantium")
+
+; partial useful when you're repeating the same combination of function and args
+(defn lousy-logger
+  [log-level message]
+  (condp = log-level
+    :warn (clojure.string/lower-case message)
+    :emergency (clojure.string/upper-case message)))
+
+(def warn (partial lousy-logger :warn))
+
+(warn "red light ahead!")                                   ; identical to calling (lousy-logger :warn "Red light ahead!")
+
+
+;; Complement
+;;===========
+(defn identify-humans [social-security-numbers]
+  (filter #(not (vampire? %)))
+  (map vampire-related-details social-security-numbers))
+
+(def not-vampire? (complement vampire?))
+(defn identify-humans [social-security-numbers]
+  (filter not-vampire? (map vampire-related-details social-security-numbers)))
