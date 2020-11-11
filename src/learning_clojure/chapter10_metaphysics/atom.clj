0870b9b91d9372f65df08a7cb04f62c9957d08ca
Robert Barr <robertbarr.uk@gmail.com>
2020-11-11 16:06:48 +0000

    futures, macros and alchemy

--- src/learning_clojure/chapter10_metaphysics/atom.clj
+++ src/learning_clojure/chapter10_metaphysics/atom.clj
@@ -38,4 +38,51 @@
 ;; watches and validators
 ;;=======================
 
+(defn shuffle-speed
+  [zombie]
+  (* (:cuddle-hunger zombie)
+     (-100 (:percent-deteriorated zombie))))
+
+(defn shuffle-alert
+  [key watched old-state new-state]
+  (let [sph (shuffle-speed new-state)]
+    (if (> sph 500)
+      (do
+        (println "Run!")
+        (println "sph is now " sph)
+        (println "This message from " key))
+      (do
+        (println "Alls well with " key)
+        (println "cuddle hunger: " (:cuddle-hunger-level new-state))
+        (println "percent deteriorated " (:percent-deteriorated new-state))
+        (println "SPH " sph)))))
+
+;; watch functions take 4 args:
+;;-- key for reporting, atom being watched, the state before and state after
+
+(reset! fred {:cuddle-hunger-level 22
+              :percent-deteriorated 2})
+(add-watch fred :fred-shuffle-alert shuffle-alert)          ;; add watch with :fred-shuffle-alert key
+(swap! fred update-in [:percent-deteriorated] + 1)          ;; update and trigger watch
+
+
+;; validators
+;;===========
+;; allow you to specify what states are allowable for a reference
+(defn percent-deteriorated-validator
+  [{:keys [percent-deteriorated]}]
+  (and (>= percent-deteriorated 0)
+       (<= percent-deteriorated 100)))
+
+;; only takes one arg. if validator fails by returning false or throwing an exception
+
+(def bobby
+  atom {:cuddle-hunger-level 0 :percent-deteriorated 0}
+  :validator percent-deteriorated-validator)
+
+(swap! bobby update-in [:percent-deteriorated] + 200)
+
+
+;; transaction semantics with REFz
+;; provides atomicity, consistency nd isolation
 
